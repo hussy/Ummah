435c426f2a9076900411fd290f28dd2ff6957862
Salem Abuammer <sabuammer@Salems-MacBook-Pro.local>
2025-02-21 09:09:56 -0600

    update events

--- application/views/home.php
+++ application/views/home.php
@@ -1,14 +1,14 @@
 <div class="ummah-banner-section">
     <div class="video-overlay"></div>
     <video autoplay muted loop playsinline id="ummah-video">
-        <source src="<?php echo ASSETS_URL;?>video/videoplayback.mp4" type="video/mp4">
+        <source src="<?php echo ASSETS_URL; ?>video/videoplayback.mp4" type="video/mp4">
         Your browser does not support HTML5 video.
     </video>
     <div class="banner-content">
         <div class="banner-heading">Welcome to the UMMA</div>
         <div class="banner-text">A community space modeled after the Prophet PBUH ﷺ</div>
         <div class="banner-button">
-            <a href="<?php echo WEBSITE_URL;?>join-umma">
+            <a href="<?php echo WEBSITE_URL; ?>join-umma">
                 <button class="btn btn-banner btn-primary">Join the UMMA</button>
             </a>
         </div>
@@ -17,7 +17,9 @@
         <div class="marquee-box">
             <div class="row">
                 <div class="col-lg-3 col-md-4 col-sm-5 col-6">
-                    <div class="marquee-time"><img src="<?php echo ASSETS_URL;?>images/clock.png"><span>This week’s</sspan></div>
+                    <div class="marquee-time"><img src="<?php echo ASSETS_URL; ?>images/clock.png"><span>This week’s
+                            </sspan>
+                    </div>
                 </div>
                 <div class="col-lg-9 col-md-8 col-sm-7 col-6">
                     <ul class="prayer-times">
@@ -45,10 +47,12 @@
                     <div class="featured-sub-heading">Winter Semester</div>
                     <div class="featured-punch-line">Open the Door to Sacred Knowledge</div>
                     <div class="image">
-                        <img src="<?php echo ASSETS_URL;?>images/home-featured.jpg" class="img img-fluid" alt="Featured Img" />
+                        <img src="<?php echo ASSETS_URL; ?>images/home-featured.jpg" class="img img-fluid"
+                            alt="Featured Img" />
                         <div class="background-image"></div>
                     </div>
-                    <div class="featured-text">The UMMA, in collaboration with Miftaah Institute, presents the Miftaah Lite Program for youth ages 15+ and adults. This winter semester features:</div>
+                    <div class="featured-text">The UMMA, in collaboration with Miftaah Institute, presents the Miftaah
+                        Lite Program for youth ages 15+ and adults. This winter semester features:</div>
                     <div class="featured-points">
                         <div class="points">
                             <div class="row">
@@ -65,7 +69,9 @@
                         <div class="points">
                             <div class="row">
                                 <div class="col-lg-4 head">Instructors:</div>
-                                <div class="col-lg-8 text last">Shaykh Abdullah Waheed & Shaykh Mohamed Almasmari Classes are held every Monday from 7-9 PM. Enjoy 20% off with code UMMA20 and contact us for financial aid options!</div>
+                                <div class="col-lg-8 text last">Shaykh Abdullah Waheed & Shaykh Mohamed Almasmari
+                                    Classes are held every Monday from 7-9 PM. Enjoy 20% off with code UMMA20 and
+                                    contact us for financial aid options!</div>
                             </div>
                         </div>
                     </div>
@@ -78,10 +84,10 @@
             </div>
             <div class="col-lg-6">
                 <div class="right-featured-home">
-                     <div class="image">
-                        <img src="<?php echo ASSETS_URL;?>images/home-featured.jpg" alt="Featured Img" />
+                    <div class="image">
+                        <img src="<?php echo ASSETS_URL; ?>images/home-featured.jpg" alt="Featured Img" />
                         <div class="background-image"></div>
-                     </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -113,7 +119,7 @@
         <div class="col-lg-6 col-md-7">
             <div class="right-section"> 
                 <div class="image-frame">
-                    <img src="<?php echo ASSETS_URL;?>images/lite2.jpg" alt="Featured Img" />
+                    <img src="<?php echo ASSETS_URL; ?>images/lite2.jpg" alt="Featured Img" />
                 </div>
                 <div class="bottom-section">
                     <div class="heading">Weekly Classes</div>
@@ -141,124 +147,11 @@
         <div class="featured-programs">Upcoming Programs</div>
         <div class="featured-blocks">
             <div class="blocks">
-                <div class="event-time">Sat  |  Jan 25  |  9:00 PM</div>
+                <div class="event-time">Fri | Feb 21 | 05:00 AM</div>
                 <div class="event-card">
                     <div class="event-image">
-                        <img src="<?php echo ASSETS_URL;?>images/up-event1.png" class="img">
-                    </div>
-                    <div class="event-title">
-                        Brunch + Islamic-Chinese<br> Caligraphy
-                    </div>
-                    <div class="event-sheikh">
-                        Master Haji Noor
-                    </div>
-                    <div class="event-button">
-                        <a href="https://linktr.ee/michiganumma" target="_blank">
-                            <button class="btn btn-program-featured btn-primary">Register</button>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div class="blocks">
-                <div class="event-time">Fri  |  Jan 31  |  7:30 PM</div>
-                <div class="event-card">
-                    <div class="event-image">
-                        <img src="<?php echo ASSETS_URL;?>images/up-event2.png" class="img">
-                    </div>
-                    <div class="event-title">
-                        Islamophobia <br> Interfaith
-                    </div>
-                    <div class="event-sheikh">
-                        Mark Crain
-                    </div>
-                    <div class="event-button">
-                        <a href="https://linktr.ee/michiganumma" target="_blank">
-                            <button class="btn btn-program-featured btn-primary">Register</button>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div class="blocks">
-                <div class="event-time">Sat  |  Feb 07  |  05:00 AM</div>
-                <div class="event-card">
-                    <div class="event-image">
-                        <img src="<?php echo ASSETS_URL;?>images/up-event4.png" class="img">
-                    </div>
-                    <div class="event-title">
-                        The UMMA Ramadan<br> Reset
-                    </div>
-                    <div class="event-sheikh">
-                        Imam Khalid Latif
-                    </div>
-                    <div class="event-button">
-                        <a href="https://linktr.ee/michiganumma" target="_blank">
-                            <button class="btn btn-program-featured btn-primary">Register</button>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div class="blocks">
-                <div class="event-time">Fri  |  Feb 14  |  06:30 PM</div>
-                <div class="event-card">
-                    <div class="event-image">
-                        <img src="<?php echo ASSETS_URL;?>images/fundraising-dinner.jpeg" class="img image-fit">
-                    </div>
-                    <div class="event-title">
-                        UMMA Community <br>Fundraising Dinner
-                    </div>
-                    <div class="event-sheikh">
-                        &nbsp;
-                    </div>
-                    <div class="event-button">
-                        <a href="https://linktr.ee/michiganumma" target="_blank">
-                            <button class="btn btn-program-featured btn-primary">Register</button>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div class="blocks">
-                <div class="event-time">Sat  |  Feb 1  |  11:00 AM</div>
-                <div class="event-card">
-                    <div class="event-image">
-                        <img src="<?php echo ASSETS_URL;?>images/souk-al-umma-event-flyer.jpeg" class="img image-fit">
-                    </div>
-                    <div class="event-title">
-                        Souq Al-UMMA <br>Ramadan Bazaar
-                    </div>
-                    <div class="event-sheikh">
-                        &nbsp;
-                    </div>
-                    <div class="event-button hide-button">
-                        <a href="https://linktr.ee/michiganumma" target="_blank">
-                            <button class="btn btn-program-featured btn-primary">Register</button>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div class="blocks">
-                <div class="event-time">Sat  |  Feb 15  |  11:30 PM</div>
-                <div class="event-card">
-                    <div class="event-image">
-                        <img src="<?php echo ASSETS_URL;?>images/event6.jpeg" class="img">
-                    </div>
-                    <div class="event-title">
-                        Fiqh of <br> Zakat
-                    </div>
-                    <div class="event-sheikh">
-                        Mft. Aasim Rashid
-                    </div>
-                    <div class="event-button">
-                        <a href="https://linktr.ee/michiganumma" target="_blank">
-                            <button class="btn btn-program-featured btn-primary">Register</button>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div class="blocks">
-                <div class="event-time">Fri  |  Feb 21  |  05:00 AM</div>
-                <div class="event-card">
-                    <div class="event-image">
-                        <img src="<?php echo ASSETS_URL;?>images/up-event7.png" class="img">
+                        <img src="<?php echo ASSETS_URL; ?>images/sisters-qiyam.png" class="img"
+                            style="object-fit: contain;">
                     </div>
                     <div class="event-title">
                         Pre-Ramadan Reset <br>Sisters Qiyam
@@ -267,7 +160,23 @@
                         Shaykha Dr. Haifaa Younis
                     </div>
                     <div class="event-button">
-                        <a href="https://linktr.ee/michiganumma" target="_blank">
+                        <a href="https://www.zeffy.com/en-US/ticketing/womens-qiyam" target="_blank">
+                            <button class="btn btn-program-featured btn-primary">Register</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="blocks">
+                <div class="event-time">Sat | Feb 22 | 05:00 PM</div>
+                <div class="event-card">
+                    <div class="event-image">
+                        <img src="<?php echo ASSETS_URL; ?>images/once-upon-a-time-makkah.jpeg" class="img">
+                    </div>
+                    <div class="event-title">
+                        Once Upon a Time in Makkah <br>A Theatrical Journey Through the Seerah
+                    </div>
+                    <div class="event-button">
+                        <a href="https://www.almaghrib.org/makkah/" target="_blank">
                             <button class="btn btn-program-featured btn-primary">Register</button>
                         </a>
                     </div>
@@ -283,7 +192,7 @@
             <div class="box">
                 <div class="program-schedule">Friday | Jan 10 | 7:00 PM</div>
                 <div class="box-wrapper">
-                    <div class="image"><img src="<?php echo ASSETS_URL;?>images/majesty.jpeg"></div>
+                    <div class="image"><img src="<?php echo ASSETS_URL; ?>images/majesty.jpeg"></div>
                     <div class="title">His Majesty - Unlocking the Names of Allah</div>
                     <p class="name-age">Shaykh Ammar AlShukry</p>
                     <div class="program-button">
@@ -296,7 +205,7 @@
             <div class="box">
                 <div class="program-schedule">Monday | Jan 06 | 7:00 PM</div>
                 <div class="box-wrapper">
-                    <div class="image"><img src="<?php echo ASSETS_URL;?>images/lite.jpeg"></div>
+                    <div class="image"><img src="<?php echo ASSETS_URL; ?>images/lite.jpeg"></div>
                     <div class="title">Miftaah Lite Program - UMMA</div>
                     <p class="name-age">Shaykh Mohamed Almasmari & Shaykh Abdullah Waheed</p>
                     <a href="https://linktr.ee/michiganumma" target="_blank">
@@ -312,6 +221,10 @@
     <div class="mission-content">
         <p>Welcome to the UMMA—the community space modeled after the prophetic society of Prophet Muhammad ﷺ.</p>
         <p>&nbsp;</p>
-        <p>At the heart of Michigan’s Muslim community, the UMMA stands as a central hub, where youth, families, & individuals gather to deepen their faith, cultivate spiritual growth, and create lasting social bonds. Through reviving the Sunnah, we draw wisdom from our past to build a thriving community in the present, while laying the foundation for the future. The aim is to build a broader society rooted in our shared values, inspiring generations for years to come.</p>
+        <p>At the heart of Michigan’s Muslim community, the UMMA stands as a central hub, where youth, families, &
+            individuals gather to deepen their faith, cultivate spiritual growth, and create lasting social bonds.
+            Through reviving the Sunnah, we draw wisdom from our past to build a thriving community in the present,
+            while laying the foundation for the future. The aim is to build a broader society rooted in our shared
+            values, inspiring generations for years to come.</p>
     </div>
 </div>
